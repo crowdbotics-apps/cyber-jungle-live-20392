51e818fa8f1d987b39865cf6df554932e26ef1ae
Crowdbotics <team@crowdbotics.com>
2020-09-17 20:56:34 +0000

    Update models

--- backend/cyber_jungle_live_20392/wsgi.py
+++ backend/cyber_jungle_live_20392/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyber_jungle_live_20392.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cyber_jungle_live_20392.settings")
 
 application = get_wsgi_application()
